fcc0ed28ade7b99961013f533da05ef1260cd479
coderman001 <290903990@qq.com>
2018-05-15 16:15:52 +0800

    Initial commit

---
+++ app/src/main/java/com/aaa/simpleapp/MainActivity.java
@@ -0,0 +1,16 @@
+package com.aaa.simpleapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.alibaba.fastjson.JSON;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        JSON.parse("{\"username\":\"bruse\"}");
+    }
+}
